b175f29d056fa6abb0cd99e20bed170d9ea38a56
explosion-mental <explosion0mental@gmail.com>
2022-02-21 16:05:05 -0500

    fix 'bug' (not intended behaviour) with cyclelayout patch, this didn't allow to 'cycle back' to the last layout, but intead to the `last - 1` layout.

--- dwm.c
+++ dwm.c
@@ -1164,7 +1164,7 @@ cyclelayout(const Arg *arg)
 		if (l != layouts && (l - 1)->symbol)
 			setlayout(&((Arg) { .v = (l - 1) }));
 		else
-			setlayout(&((Arg) { .v = &layouts[LENGTH(layouts) - 2] }));
+			setlayout(&((Arg) { .v = &layouts[LENGTH(layouts) - 1] }));
 	}
 }
 
