268c4cdc7ccadaa2862400d37c06caf1c5416926
explosion-mental <explosion0mental@gmail.com>
2022-04-28 20:17:20 -0500

    make some variables const

--- config.h
+++ config.h
@@ -199,10 +199,10 @@ static const int scrollargs[4][2] = {
 };
 
 /* layout(s) */
-static float mfact     = 0.55;	/* factor of master area size [0.05..0.95] */
-static int nmaster     = 1;	/* number of clients in master area */
-static int resizehints = 0;	/* 1 means respect size hints in tiled resizals */
-static int floathints  = 0;	/* 1 means respect size hints if the window is floating */
+static const float mfact     = 0.55;	/* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;	/* number of clients in master area */
+static const int resizehints = 0;	/* 1 means respect size hints in tiled resizals */
+static const int floathints  = 0;	/* 1 means respect size hints if the window is floating */
 
 static const Layout layouts[] = {
 	/* symbol	arrange function   gaps (enabled if omitted)			*/
