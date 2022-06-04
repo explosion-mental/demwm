1cd6dfec49b5754fe2f2b6344c104ac03d0738b6
explosion-mental <explosion0mental@gmail.com>
2022-06-03 20:30:17 -0500

    replace deprecated function hmmm.. X is deprecated!

--- dwm.c
+++ dwm.c
@@ -35,6 +35,7 @@
 #include <X11/cursorfont.h>
 #include <X11/keysym.h>
 #include <X11/Xatom.h>
+#include <X11/XKBlib.h>
 #include <X11/Xlib.h>
 #include <X11/Xproto.h>
 #include <X11/Xutil.h>
@@ -2196,7 +2197,8 @@ keypress(XEvent *e)
 	XKeyEvent *ev;
 
 	ev = &e->xkey;
-	keysym = XKeycodeToKeysym(dpy, (KeyCode)ev->keycode, 0);
+	keysym = XkbKeycodeToKeysym(dpy, ev->keycode, 0, 0);
+
 	for (i = 0; i < LENGTH(keys); i++)
 		if (keysym == keys[i].keysym
 		&& CLEANMASK(keys[i].mod) == CLEANMASK(ev->state)
