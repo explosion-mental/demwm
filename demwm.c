c6c3510f31b3cf3abe9fdfff2fadb1197597fe48
explosion-mental <explosion0mental@gmail.com>
2022-06-13 12:14:42 -0500

    change the first comment, be more straightforward and give places to look for

--- demwm.c
+++ demwm.c
@@ -1,24 +1,29 @@
 /* See LICENSE file for copyright and license details.
  *
- * dynamic window manager is designed like any other X client as well. It is
- * driven through handling X events. In contrast to other X clients, a window
- * manager selects for SubstructureRedirectMask on the root window, to receive
- * events about window (dis-)appearance. Only one X connection at a time is
- * allowed to select for this event mask.
- *
- * The event handlers of dwm are organized in an array which is accessed
- * whenever a new event has been fetched. This allows event dispatching
- * in O(1) time.
- *
  * Each child of the root window is called a client, except windows which have
  * set the override_redirect flag. Clients are organized in a linked client
  * list on each monitor, the focus history is remembered through a stack list
  * on each monitor. Each client contains a bit array to indicate the tags of a
  * client.
  *
- * Keys and tagging rules are organized as arrays and defined in config.h.
+ * Keys, Buttons and Rules are organized as arrays and defined in config.h.
  *
- * To understand everything else, start reading main().
+ * Most of dwm/demwm codebase is around handling X events, thus being a window
+ * manager. We access the X server by the good old Xlib library (demwm uses the
+ * xcb lib to get the pid, swallow patch). Whenever we need to handle an event,
+ * we call handler, array that contains the calls and function to handle them
+ * (how to handle them).
+ *
+ * Most of that X stuff you won't need to touch, unless you plan on
+ * refactoring, go to golf, logic hunting and all for the precious (most likely
+ * micro) optimizations. The functions that take the struct type Arg as an
+ * argument is the 'user functions', you can take a look how they do things.
+ *
+ * Every window that is spawned goes through by manage(), so it is another good
+ * start.
+ *
+ * To understand everything else, start reading main(),
+ * www.x.org/docs/X11/xlib.pdf and the comments I left, happy hacking.
  */
 #include <errno.h>
 #include <locale.h>
