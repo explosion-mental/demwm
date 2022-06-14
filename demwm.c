eaa1f3e9fe9c02f05476ac2757381b1b00242436
explosion-mental <explosion0mental@gmail.com>
2022-06-14 09:22:32 -0500

    fix for notification windows

--- demwm.c
+++ demwm.c
@@ -2893,8 +2893,8 @@ restack(Monitor *m)
 	if (!m->sel)
 		return;
 	/* unfocus floating */
-	//if (m->sel->isfloating || !m->lt[m->sellt]->arrange)
-	XRaiseWindow(dpy, m->sel->win);
+	if (m->sel->isfloating || !m->lt[m->sellt]->arrange)
+		XRaiseWindow(dpy, m->sel->win);
 	if (m->lt[m->sellt]->arrange) {
 		wc.stack_mode = Below;
 		wc.sibling = m->barwin;
