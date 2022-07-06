19c14c8f6b77e783e1773703d1cdd54554000eca
explosion-mental <explosion0mental@gmail.com>
2022-07-06 17:31:16 -0500

    only center when client is float or floating layout

--- demwm.c
+++ demwm.c
@@ -2401,9 +2401,12 @@ manage(Window w, XWindowAttributes *wa)
 	c->y = MAX(c->y, ((c->mon->by == c->mon->my) && (c->x + (c->w / 2) >= c->mon->wx)
 		&& (c->x + (c->w / 2) < c->mon->wx + c->mon->ww)) ? bh : c->mon->my);
 	c->bw = (c->f & Float) ? fborderpx : borderpx;
+
 	/* center the window (floating) */
-	c->x = c->mon->mx + (c->mon->mw - WIDTH(c)) / 2;
-	c->y = c->mon->my + (c->mon->mh - HEIGHT(c)) / 2;
+	if (c->f & Float || !c->mon->lt->arrange) {
+		c->x = c->mon->mx + (c->mon->mw - WIDTH(c)) / 2;
+		c->y = c->mon->my + (c->mon->mh - HEIGHT(c)) / 2;
+	}
 
 	wc.border_width = c->bw;
 	XConfigureWindow(dpy, w, CWBorderWidth, &wc);
