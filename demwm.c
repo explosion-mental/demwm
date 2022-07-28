5e3233d58abb87a7907bee87510a53a6795f6b76
explosion-mental <explosion0mental@gmail.com>
2022-07-28 12:29:01 -0500

    remove already included headers
    these headers files are already included in util.c

--- demwm.c
+++ demwm.c
@@ -29,10 +29,6 @@
 #include <locale.h>
 #include <poll.h>
 #include <signal.h>
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
