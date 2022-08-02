c822fb8eb2fc4bdba6dc7faded71c4e2c522f267
explosion-mental <explosion0mental@gmail.com>
2022-08-02 16:11:11 -0500

    add missing declarations for swallow() and unswallow()

--- demwm.c
+++ demwm.c
@@ -460,6 +460,8 @@ static pid_t getparentprocess(pid_t p);
 static int isdescprocess(pid_t p, pid_t c);
 static Client *swallowingclient(Window w);
 static Client *termforwin(const Client *c);
+static void swallow(Client *p, Client *c);
+static void unswallow(Client *c);
 static pid_t winpid(Window w);
 
 /* variables */
