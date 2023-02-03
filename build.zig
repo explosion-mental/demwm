2a1b53511afe554c899c8a13345514c6a7afc9d0
explosion-mental <explosion0mental@gmail.com>
2023-02-02 19:45:03 -0500

    build.zig: add system libs with a for loop

--- build.zig
+++ build.zig
@@ -2,18 +2,26 @@ const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
     const exe = b.addExecutable("demwm", null);
+    const libs: []const u8 = &.{
+        "imlib2",
+        "x11-xcb",
+        "xcb",
+        "xcb-res",
+        "fontconfig",
+        "X11",
+        "Xft",
+        "Xrender",
+        "Xinerama",
+    };
     exe.addCSourceFiles(&.{ "demwm.c", "util.c", "drw.c" }, &.{ "-Wall", "-DVERSION=\"1.0\"" });
     exe.linkLibC();
+
+    // INCS = -I${X11INC} -I${FREETYPEINC}
     exe.addIncludePath("/usr/X11R6/include");
     exe.addLibraryPath("/usr/X11R6/lib");
-    exe.linkSystemLibrary("imlib2");
-    exe.linkSystemLibrary("x11-xcb");
-    exe.linkSystemLibrary("xcb");
-    exe.linkSystemLibrary("xcb-res");
-    exe.linkSystemLibrary("fontconfig");
-    exe.linkSystemLibrary("Xft");
-    exe.linkSystemLibrary("Xrender");
-    exe.linkSystemLibrary("X11");
-    exe.linkSystemLibrary("Xinerama");
+
+    // external libs
+    for (libs) |lib| exe.linkSystemLibrary(lib);
+
     exe.install();
 }
